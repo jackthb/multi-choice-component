f5345bbe1e283cdd13f4f7c3511b476e1006b986
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-15 19:36:03 +0100

    change data format

--- src/data.tsx
+++ src/data.tsx
@@ -1,25 +1,65 @@
 type Question = {
   question: string;
   option: {
-    correct: string;
-    wrong: string;
-  }[];
+    label: string;
+    isCorrect: boolean;
+  }[][];
 };
 
 export const defaultOptions: Question = {
   question: "What are the ideal conditions inside an office?",
   option: [
-    { correct: "good pay", wrong: "bad pay" },
-    { correct: "lot of meetings", wrong: "less meetings" },
-    { correct: "free coffee", wrong: "expensive coffee)" },
-    { correct: "dog in office", wrong: "bear in office" },
+    [
+      { label: "Good Pay", isCorrect: true },
+      { label: "Bad pay", isCorrect: false },
+    ],
+    [
+      { label: "lot of meetings", isCorrect: true },
+      { label: "less meetings", isCorrect: false },
+    ],
+    [
+      { label: "free coffee", isCorrect: true },
+      { label: "expensive coffee", isCorrect: false },
+    ],
+    [
+      { label: "dog in office", isCorrect: true },
+      { label: "bear in office", isCorrect: false },
+    ],
+  ],
+};
+
+export const animalCell: Question = {
+  question: "An animal cell contains",
+  option: [
+    [
+      { label: "Ribosomes", isCorrect: true },
+      { label: "Cell wall", isCorrect: false },
+    ],
+    [
+      { label: "Cytoplasm", isCorrect: true },
+      { label: "Chloroplast", isCorrect: false },
+    ],
+    [
+      { label: "Partially permeable membrane", isCorrect: true },
+      { label: "Impermeable membrane", isCorrect: false },
+    ],
+    [
+      { label: "Mitochondria", isCorrect: true },
+      { label: "Cellulose", isCorrect: false },
+    ],
   ],
 };
 
 export const altOptions: Question = {
   question: "What day is it today?",
   option: [
-    { correct: "The day is", wrong: "The month is" },
-    { correct: "Saturday", wrong: "Sunday" },
+    [
+      { label: "The day is", isCorrect: true },
+      { label: "The month is", isCorrect: false },
+    ],
+    [
+      { label: "Saturday", isCorrect: true },
+      { label: "Sunday", isCorrect: false },
+    ],
   ],
 };
