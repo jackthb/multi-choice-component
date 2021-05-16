2f2bc42b8e8e7ef9cff744a4cfc4806e53364dce
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-16 22:16:38 +0100

    select any question

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
+import { useState, useEffect } from "react";
 import MultiChoice from "./components/MultiChoice";
 
-import { animalCell as Question } from "./data";
+import { animalCell, defaultOptions, altOptions } from "./data";
 
 export default function App() {
-  Question.option.forEach((x) => {
-    x.sort(() => Math.random() - 0.5);
-  });
+  const [question, setQuestion] = useState(animalCell);
+  const [sorted, setSorted] = useState(false);
+
+  useEffect(() => {
+    if (sorted) return;
+    question.option.forEach((x) => {
+      x.sort(() => Math.random() - 0.5);
+    });
+    setSorted(true);
+  }, [question]);
 
   return (
     <div className="container">
-      <MultiChoice question={Question.question} option={Question.option} />
+      <MultiChoice
+        key={question.id}
+        id={question.id}
+        question={question.question}
+        option={question.option}
+      />
+
+      <div className="chooseQuestion">
+        <span>Choose Question:</span>
+        <ul>
+          <li onClick={() => setQuestion(animalCell)}>Animal Cell question</li>
+          <li onClick={() => setQuestion(defaultOptions)}>
+            Office Life question
+          </li>
+          <li onClick={() => setQuestion(altOptions)}>Calendar question</li>
+        </ul>
+      </div>
+
       {/* <MultiChoice
         question="How many trees are there in England"
         option={[
