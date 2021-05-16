d473891148cacd6693ca258b6a837bb60a17ed72
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-16 16:47:51 +0100

    example for inline question

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import MultiChoice from "./components/MultiChoice";
 import { animalCell as Question } from "./data";
 
 export default function App() {
-  Question.option.map((x) => {
+  Question.option.forEach((x) => {
     x.sort(() => Math.random() - 0.5);
   });
 
   return (
     <div className="container">
       <MultiChoice question={Question.question} option={Question.option} />
+      {/* <MultiChoice
+        question="How many trees are there in England"
+        option={[
+          [
+            { label: "there are no trees", isCorrect: true },
+            { label: "there are many", isCorrect: false },
+          ],
+          [
+            { label: "tall trees", isCorrect: true },
+            { label: "short trees", isCorrect: false },
+          ],
+        ]}
+      /> */}
     </div>
   );
 }
