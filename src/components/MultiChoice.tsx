ea74128160c1e2f6b6e78e070c1be37ba006b1d6
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-16 16:48:13 +0100

    change bg colour on correct

--- src/components/MultiChoice.tsx
+++ src/components/MultiChoice.tsx
@@ -20,14 +20,35 @@ function Answer({ correct }: AnswerProps) {
 export default function MultiChoice({ question, option }: MultiChoiceProps) {
   const [correct, setCorrect] = useState<boolean>(false);
   const [noCorrect, setNoCorrect] = useState<number>(0);
-
+  const [colour, setColour] = useState<string>(
+    "linear-gradient(to bottom, rgba(250, 145, 97, 0.7), rgba(247, 59, 28, 0.69))"
+  );
   useEffect(() => {
     if (noCorrect === option.length) {
       setCorrect(true);
     }
-  }, [noCorrect]);
+    switch (noCorrect) {
+      case 1:
+        setColour("linear-gradient(to bottom, #FDD819, #E80505)");
+        break;
+      case 2:
+        setColour("linear-gradient(to bottom, #FEC163, #DE4313)");
+        break;
+      case 3:
+        setColour("linear-gradient(to bottom, #ffd392, #DE4313)");
+        break;
+      case 4:
+        setColour("linear-gradient(to bottom, #47e4c1, #07cddd)");
+        break;
+    }
+  }, [noCorrect, option.length]);
   return (
-    <div className="Mask">
+    <div
+      className="Mask"
+      style={{
+        backgroundImage: colour,
+      }}
+    >
       <h1 className="text">{question}</h1>
       {option.map((x, index) => (
         <Slider
